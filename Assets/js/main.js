3345e5e0f5e43aea08335e34e1e53135dce8df90
Shaduli <shaduliptmjy2001@gmail.com>
2023-10-07 17:21:10 +0530

    Navbar changes corrected

---
+++ Assets/js/main.js
@@ -0,0 +1,15 @@
+// navbar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0px";
+    document.getElementById("navbar").style.backgroundColor="tranparent";
+  } else {
+    document.getElementById("navbar").style.top = "-180px";
+    document.getElementById("navbar").style.backgroundColor="tranparent";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// navbar
